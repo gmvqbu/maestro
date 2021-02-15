ed8fdce8a3801e08e1771fc28238d6776208d589
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-15 14:13:29 +0100

    add : dispatcher.js
    Création du dispatcher pour gérer les messages et les commandes

---
+++ Maestro/src/dispatcher.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class MaestroDispatcher {
+
+    constructor(client, registry) {
+        this.client = client;
+        this.registry = registry;
+    }
+
+    handleMessage(message) {
+
+    }
+
+}
+
+module.exports = MaestroDispatcher;
