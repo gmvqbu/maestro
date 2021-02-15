dac55587cd2e0decaa11cc1584eb420f1a79f9fb
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-15 21:45:01 +0100

    maj : dispatcher.js
    Début de la logique du dispatcher
    
    L'auteur du msg n'est pas un bot
    et
    le message match le pattern

--- Maestro/src/dispatcher.js
+++ Maestro/src/dispatcher.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { match } = require('../util/util');
+
 class MaestroDispatcher {
 
     constructor(client, registry) {
@@ -8,7 +10,17 @@ class MaestroDispatcher {
     }
 
     handleMessage(message) {
+        if (message.author.bot) return;
+        if (match(message.content, 'COMMAND_PATTERN', this.client.prefix)) {
+            message.reply('Bien reçu !');
+        }
+        /*
 
+        vérifier le match
+
+        si match : fetch la commande
+
+        */
     }
 
 }
