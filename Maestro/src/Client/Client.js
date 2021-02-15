8731821b84682999b03efaa2c59cc064f6a95bea
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-15 13:59:44 +0100

    maj : MaestroClient
    Passage des attributs de privé à public

--- Maestro/src/Client/Client.js
+++ Maestro/src/Client/Client.js
@@ -1,25 +1,42 @@
 'use strict';
 
 const { Client } = require("discord.js");
+const User = require("../../infrastructure/User/User");
 
 class MaestroClient extends Client {
 
     constructor(data = {}) {
+        super()
 
         /**
          * Le préfixe du bot
          * @readonly
          * @type {string}
          */
-        this.#prefix = 'prefix' in data ? data.prefix : null;
+        this.prefix = 'prefix' in data ? data.prefix : '$';
 
         /**
-         * L'id du propriétaire du bot
+         * L'id propriétaire du bot
          * @readonly
          * @type {int}
          */
-        this.#owner = 'owner' in data ? data.owner : null;
+        this.ownerID = 'ownerID' in data ? data.ownerID : null;
 
+        /**
+         * Le propriétaire du bot
+         * @readonly
+         * @type {User}
+         */
+        this.owner = this.ownerID ? this.#fetchOwner(data.ownerID) : null;
+
+    }
+
+    async #fetchOwner(ownerID) {
+        const userIDRegExp = /^\d+$/;
+        if (!ownerID.match(userIDRegExp)) throw Error(`The provided owner ID is incorrect.`);
+        const user = await this.users.fetch(ownerID);
+        if (!user) throw Error(`Could not find the bot owner.`)
+        return new User(this, user);
     }
 
 }
