0ae9d4bb1c62c9bb91b5f9f0a5411b5fbd7a3443
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-16 12:33:16 +0100

    maj : index.js
    Mise à jour du fichier avec les nouveaux path vers les services.

--- Maestro/index.js
+++ Maestro/index.js
@@ -5,9 +5,13 @@ module.exports = {
     Client: require('./src/client'),
     Dispatcher: require('./src/dispatcher'),
     Registry: require('./src/registry'),
-    Config: require('./src/Config/Config'),
+
+    // Config
+    Config: require('./src/Config/ConfigService'),
+
+    // App
     Message: require('./src/Message/Message'),
 
     // Tools
-    User: require('./infrastructure/User/User')
+    User: require('./infrastructure/User/UserService')
 }
