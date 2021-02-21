a030276b0f40b360fa5839d0d00d27ca2c598e8c
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-21 13:52:24 +0100

    maj : BaseCommand
    Refactoring : vérification des données

--- Maestro/commands/Base.js
+++ Maestro/commands/Base.js
@@ -2,33 +2,49 @@
 
 module.exports = class BaseCommand {
 
+    /**
+     * The current client
+     * @type {MaestroClient}
+     */
+    client;
+
+    /**
+     * @typedef {Object} CommandInfo
+     * @property {string} name The command name
+     * @property {string} group The command group
+     * @property {Array<string>} alias Command alias/aliases
+     */
+    /**
+     * @param {MaestroClient} client
+     * @param {CommandInfo} data
+     */
     constructor(client, data) {
+        Object.defineProperty(this, 'client', { value: client });
 
         /**
-         * The Maestro client
-         * @type {MaestroClient}
-         */
-        this.client = client;
-
-        if (!data.name) throw Error(`You must provide a command name.`)
-        /**
-         * The command name
+         * This command name
          * @type {string}
          */
-        this.name = data.name;
+        this.name = 'name' in data ? data.name : null;
+        if (!this.name) throw Error(`Command name cannot be null.`);
+        if (typeof data.name != 'string') throw Error(`Command name must be a string.`);
+        if (data.name != data.name.toLowerCase()) throw Error(`Command name must be in lower case.`);
 
-        if (!data.group) throw Error(`You must provide a command group.`)
         /**
-         * The command group
+         * This command group
          * @type {string}
          */
-        this.group = data.group;
+        this.group = 'group' in data ? data.group : null;
+        if (!this.group) throw Error(`Command group cannot be null.`);
+        if (typeof data.group != 'string') throw Error(`Command group must be a string.`);
+        if (data.group != data.group.toLowerCase()) throw Error(`Command group must be in lower case.`);
 
         /**
-         * The command aliases
+         * Command aliases
          * @type {Array<string>}
          */
         this.alias = 'alias' in data ? data.alias : null;
+        if (this.alias && !Array.isArray(this.alias)) throw Error(`Command alias must be an array of aliases.`);
     }
 
 }
