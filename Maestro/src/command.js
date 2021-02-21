e2597f2db94b4c4801e318ff20dd915b8a53a88a
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-21 20:22:11 +0100

    maj : command
    Ajout des arguments et de la création des argments

--- Maestro/src/command.js
+++ Maestro/src/command.js
@@ -1,6 +1,8 @@
 'use strict';
 
-class BaseCommand {
+const Argument = require("./argument");
+
+class Command {
 
     /**
      * The current client
@@ -13,6 +15,7 @@ class BaseCommand {
      * @property {string} name The command name
      * @property {string} group The command group
      * @property {Array<string>} alias Command alias/aliases
+     * @property {Array<object>} args Command arguments
      */
     /**
      * @param {MaestroClient} client
@@ -27,8 +30,8 @@ class BaseCommand {
          */
         this.name = 'name' in data ? data.name : null;
         if (!this.name) throw Error(`Command name cannot be null.`);
-        if (typeof data.name != 'string') throw Error(`Command name must be a string.`);
-        if (data.name != data.name.toLowerCase()) throw Error(`Command name must be in lower case.`);
+        if (typeof this.name != 'string') throw Error(`Command name must be a string.`);
+        if (this.name != this.name.toLowerCase()) throw Error(`Command name must be in lower case.`);
 
         /**
          * This command group
@@ -36,8 +39,8 @@ class BaseCommand {
          */
         this.group = 'group' in data ? data.group : null;
         if (!this.group) throw Error(`Command group cannot be null.`);
-        if (typeof data.group != 'string') throw Error(`Command group must be a string.`);
-        if (data.group != data.group.toLowerCase()) throw Error(`Command group must be in lower case.`);
+        if (typeof this.group != 'string') throw Error(`Command group must be a string.`);
+        if (this.group != this.group.toLowerCase()) throw Error(`Command group must be in lower case.`);
 
         /**
          * Command aliases
@@ -45,8 +48,27 @@ class BaseCommand {
          */
         this.alias = 'alias' in data ? data.alias : null;
         if (this.alias && !Array.isArray(this.alias)) throw Error(`Command alias must be an array of aliases.`);
+
+        /**
+         * Command arguments
+         * @type {Array<Argument>}
+         */
+        this.args = 'args' in data ? this.#createArguments(data.args) : null;
+
+        /**
+         * Argument count
+         * @type {int}
+         */
+        this.argsCount = this.args ? this.args.length : 0;
     }
 
+    #createArguments(args) {
+        if (!Array.isArray(args)) throw Error(`Command arguments must be an array.`);
+        for (let i = 0; i < args.length; i++) {
+            args[i] = new Argument(args[i]);
+        }
+        return args;
+    }
 }
 
-module.exports = BaseCommand;
+module.exports = Command;
