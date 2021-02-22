a858bd939cd6958a352c5a557f2986c7061f62ef
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-22 11:59:21 +0100

    maj : argument.js
    Ajout du type d'argument

--- Maestro/src/argument.js
+++ Maestro/src/argument.js
@@ -9,6 +9,7 @@ class Argument {
      * @typedef {Object} ArgumentInfo
      * @property {string} key Argument key (used to retreive the argument in the run method)
      * @property {string} label Argument label
+     * @property {string} type Argument type
      */
     /**
      * @param {ArgumentInfo} data
@@ -30,6 +31,16 @@ class Argument {
          */
         this.label = 'label' in data ? data.label : null;
         if (typeof this.key != 'string') throw Error(`Argument key must be a string`);
+
+        /**
+         * This argument type
+         * Could be (string|url|int|time)
+         * @type {string}
+         */
+        this.type = 'type' in data ? data.type : null;
+        if (!this.type) throw Error(`Argument type cannot be null`);
+        if (typeof this.type != 'string') throw Error(`Argument type must be a string`);
+        if (this.type != this.type.toLowerCase()) throw Error(`Argument type must be in lower case.`);
     }
 
 }
