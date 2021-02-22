d90dcb54856de26e51da3c32dd330ae253a218bd
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-22 11:59:03 +0100

    maj : constants
    Mise a jour de l'expression régulière COMMAND_PATTERN pour matcher tous types d'arguments.

--- Maestro/util/constants.js
+++ Maestro/util/constants.js
@@ -2,7 +2,7 @@
 
 const RegularExpressions = new Map(Object.entries({
     ID_PATTERN: new RegExp('^\\d+$'),
-    COMMAND_PATTERN: prefix => new RegExp(`^\\${prefix}\\w+(\\s+)?(\\w+\\s+)?`, 'i')
+    COMMAND_PATTERN: prefix => new RegExp(`^\\${prefix}\\w+(\\s+)?(.+)?`, 'i'),
 }));
 
 module.exports = {
