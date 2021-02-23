8284bb411ec76f3d8f9fbb0a5da0e1920f1fc9dc
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-23 18:53:05 +0100

    maj : dispatcher.js
    Mise à jour du collecteur d'arguments
    Vérifie les arguments manquants ,
    valide les arguments
    formatte les arguments pour les récupérer tous frais bien emballés dans la méthode run des différentes commandes

--- Maestro/src/dispatcher.js
+++ Maestro/src/dispatcher.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Discord = require('discord.js');
-const { matchRegex } = require('../util/util');
 
 /**
  * Dispatch tasks through the app
@@ -9,16 +8,8 @@ const { matchRegex } = require('../util/util');
  */
 class Dispatcher {
     constructor(client, registry) {
-
-        /**
-         * @type {MaestroClient}
-         */
-        this.client = client;
-
-        /**
-         * @type {Registry}
-         */
-        this.registry = registry;
+        Object.defineProperty(this, 'client', { value: client });
+        Object.defineProperty(this, 'registry', { value: registry });
     }
 
     /**
@@ -28,12 +19,19 @@ class Dispatcher {
      * @param {Discord.Message} msg
      */
     async handleMessage(msg) {
-        if (msg.author.bot) return;
-        if (matchRegex(msg.content, 'COMMAND_PATTERN', this.client.prefix)) {
-            const error = await this.parseMessage(msg, this.client.prefix);
-            if (error) return msg.reply(error);
-            msg.command.run(msg, msg.args)
-        }
+        if (!this.#shouldHandleMessage(msg)) return;
+
+        const error = await this.parseMessage(msg, this.client.prefix);
+        if (error) return msg.reply(error);
+        msg.command.run(msg, msg.args)
+    }
+
+    /**
+     * Wether the dispatcher should handle the message or not
+     * @param {Discord.Message} msg
+     */
+    #shouldHandleMessage(msg) {
+        return (!msg.author.bot && msg.content.match(new RegExp(`^\\${this.client.prefix}\\w+(\\s+)?(.+)?`, 'i'))) ? true : false;
     }
 
     /**
@@ -42,27 +40,50 @@ class Dispatcher {
      * @param {string} msg
      * @returns {ParsedContent} The parsed content
      */
-    async parseMessage(msg, prefix) {
+    parseMessage(msg, prefix) {
         const parsedContent = msg.content.substring(prefix.length).split(' ');
-        const command = await this.registry.fetchCommand(parsedContent.shift());
+        const command = this.registry.commands.get(parsedContent.shift());
         if (!command) return `Cette commande n'existe pas.`;
         Object.defineProperty(msg, 'command', { value: command, writable: false });
-        const args = this.formatArguments(parsedContent, command);
-        if (typeof args === 'number') return `Il manque ${args} argument${args > 1 ? 's' : ''}.`;
+        const args = this.collectArguments(command, parsedContent);
+        // collecter les arguments
+        // vérifier et adapter le type
+        if (typeof args === 'string') return args;  // Erreur
         Object.defineProperty(msg, 'args', { value: args, writable: false });
     }
 
-    formatArguments(args, command) {
-        const argsCount = command.argsCount;
-        // A VENIR : préciser quels arguments sont manquants dans un tableau avec le label
-        if (args.length < argsCount) return argsCount - args.length;
-        let keyedArgs = new Object();
-        for (let i = 0; i < argsCount; i++) {
-            const key = command.args[i].key;
-            // A VENIR : préparer les arguments de type ClientUser, GuildMember, VoiceChannel
-            keyedArgs[key] = args[i];
+    collectArguments(command, args) {
+        if (!Array.isArray(args)) throw Error(`Arguments must be an array.`);
+        if (args.length < command.argsCount) return this.checkMissingArguments(args, command);
+        let keyedArgs = new Array();
+        let labels = new Array()
+        for (let i = 0; i < args.length; i++) {
+            let v = args[i];
+            const arg = command.args[i];
+            const type = this.registry.types.get(arg.type);
+            if (!type.validate(v)) {
+                labels.push(arg.label);
+            } else {
+                v = type.format(v);
+                keyedArgs[arg.key] = args[i];
+            }
         }
+        if (labels.length > 0) return `je ne peux pas effectuer cette commande car ${labels.length > 1 ? 'les ' : "l'"}argument${labels.length > 1 ? 's' : ''} \`${labels.join('`, `')}\` ${labels.length > 1 ? 'ne sont' : "n'est"} pas valide${labels.length > 1 ? 's' : ''}`;
         return keyedArgs;
+    }
+
+    checkMissingArguments(args, command) {
+        const expectedCount = command.argsCount;
+        const givenCount = args.length;
+        let missing = [];
+        for (let i = givenCount; i < expectedCount; i++) {
+            missing.push(command.args[i].label)
+        }
+        return (missing.length > 1)
+            ?
+            `je ne peux pas effectuer la commande \`${command.name}\` car il manque les arguments suivants : \`${missing.join('`, `')}\``
+            :
+            `je ne peux pas effectuer cette commande \`${command.name}\` car il manque l'argument \`${String(missing)}\``;
     }
 }
 
