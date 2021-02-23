d45e4770ff4dc3fcb655b3dfbc9bd75e1346edb5
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-23 20:17:56 +0100

    add : collector.js
    Ajout d'un fichier pour collecter les arguments.
    Allègement di dispatcher en conséquence.

--- Maestro/src/dispatcher.js
+++ Maestro/src/dispatcher.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Discord = require('discord.js');
+const ArgumentCollector = require('../commands/collector');
 
 /**
  * Dispatch tasks through the app
@@ -10,6 +11,12 @@ class Dispatcher {
     constructor(client, registry) {
         Object.defineProperty(this, 'client', { value: client });
         Object.defineProperty(this, 'registry', { value: registry });
+
+        /**
+         * The argument collector
+         * @type {ArgumentCollector}
+         */
+        this.argumentCollector = new ArgumentCollector(client, registry);
     }
 
     /**
@@ -45,45 +52,10 @@ class Dispatcher {
         const command = this.registry.commands.get(parsedContent.shift());
         if (!command) return `Cette commande n'existe pas.`;
         Object.defineProperty(msg, 'command', { value: command, writable: false });
-        const args = this.collectArguments(command, parsedContent);
-        // collecter les arguments
-        // vérifier et adapter le type
+        if (command.argsCount === 0) return;
+        const args = this.argumentCollector.collect(command, parsedContent);
         if (typeof args === 'string') return args;  // Erreur
         Object.defineProperty(msg, 'args', { value: args, writable: false });
-    }
-
-    collectArguments(command, args) {
-        if (!Array.isArray(args)) throw Error(`Arguments must be an array.`);
-        if (args.length < command.argsCount) return this.checkMissingArguments(args, command);
-        let keyedArgs = new Array();
-        let labels = new Array()
-        for (let i = 0; i < args.length; i++) {
-            let v = args[i];
-            const arg = command.args[i];
-            const type = this.registry.types.get(arg.type);
-            if (!type.validate(v)) {
-                labels.push(arg.label);
-            } else {
-                v = type.format(v);
-                keyedArgs[arg.key] = args[i];
-            }
-        }
-        if (labels.length > 0) return `je ne peux pas effectuer cette commande car ${labels.length > 1 ? 'les ' : "l'"}argument${labels.length > 1 ? 's' : ''} \`${labels.join('`, `')}\` ${labels.length > 1 ? 'ne sont' : "n'est"} pas valide${labels.length > 1 ? 's' : ''}`;
-        return keyedArgs;
-    }
-
-    checkMissingArguments(args, command) {
-        const expectedCount = command.argsCount;
-        const givenCount = args.length;
-        let missing = [];
-        for (let i = givenCount; i < expectedCount; i++) {
-            missing.push(command.args[i].label)
-        }
-        return (missing.length > 1)
-            ?
-            `je ne peux pas effectuer la commande \`${command.name}\` car il manque les arguments suivants : \`${missing.join('`, `')}\``
-            :
-            `je ne peux pas effectuer cette commande \`${command.name}\` car il manque l'argument \`${String(missing)}\``;
     }
 }
 
