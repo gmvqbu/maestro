a441fa5f643a3e999ae11b5b8885dddad075749c
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-23 18:51:50 +0100

    maj : maestro.js
    Implémentation du fichier principal avec la nouvelle version du registre,
    Mise à jour après la suppression des utils

--- maestro.js
+++ maestro.js
@@ -1,22 +1,27 @@
 'use strict';
 
+const commands = require('./config/commands');
 const { Client, Config, User } = require('./Maestro/');
-const { matchString } = require('./Maestro/util/util');
+const BaseManager = require('./Maestro/src/registry/base');
 
 const client = new Client({
     prefix: Config.botPrefix,
     ownerID: Config.botOwner
 });
 
-client.registry.registerCommands([
-    require('./Maestro/commands/util/ping.js')
-]);
+new BaseManager(client, client.registry)
+
+client.registry
+    .types.register([
+        require('./Maestro/types/string')
+    ])
+    .commands.register(commands);
 
 client.on('ready', () => {
     const bot = new User(client, client.user);
     console.log(`Logged in as ${bot.tag} !`);
 
-    if (!matchString(bot.username, Config.botUsername)) bot.updateUsername(Config.botUsername);
+    if (bot.username != Config.botUsername) bot.updateUsername(Config.botUsername);
 });
 
 client.login(Config.botToken);
