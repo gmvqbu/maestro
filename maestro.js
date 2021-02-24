be50b4f14c418474652ef8baff9a16632d09db8d
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-24 20:06:49 +0100

    maj : maestro.js
    Refactoring

--- maestro.js
+++ maestro.js
@@ -14,7 +14,6 @@ client.on('ready', () => {
     // This part is not in the client file cuz it requires the config
     const bot = client.user;
     console.log(`Logged in as ${bot.tag} !`);
-
     if (bot.username != Config.username) setUsername(bot, Config.username);
 });
 
