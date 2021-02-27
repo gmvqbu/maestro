67194bd01aaae84fed8e65629332e475346c2e84
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-27 20:27:45 +0100

    add : EmbedTrack
    Ajout de EmbedTrack pour lancer un musique directement depuis un embed sans utiliser de service particulier.

---
+++ Maestro/player/tracks/EmbedTrack.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const Music = require("./BaseTrack");
+
+class EmbedTrack extends Music {
+    constructor(data) {
+        super({
+            title: data.title ?? null,
+            url: data.video.url ?? null,
+            author: {
+                name: data.author.name ?? null,
+                url: data.author.url ?? null
+            }
+        });
+    }
+}
+
+module.exports = EmbedTrack;
