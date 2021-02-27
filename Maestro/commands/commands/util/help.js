d7ed1c279a00fbcb839f15dab02fe2ab9b771899
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-27 22:43:44 +0100

    maj : help.js
    Commande scindée en 2
    une partie traite toutes les commandes : message d'aide général
    l'autre partie est spécifique à une commande : message d'aide spécialisé
    Cela implique d'ajouter l'argument commande de type commande

--- Maestro/commands/commands/util/help.js
+++ Maestro/commands/commands/util/help.js
@@ -8,20 +8,48 @@ class HelpCommand extends Command {
         super(client, {
             name: 'help',
             alias: ['h', 'aide', 'commands', 'commandes'],
-            description: "affiche ce message d'aide."
+            description: "affiche ce message d'aide.",
+            args: [
+                {
+                    key: 'command',
+                    label: 'Commande',
+                    type: 'command',
+                    default: 'help',
+                }
+            ]
         });
     }
 
-    run(msg) {
+    async run(msg, args) {
+        const cmd = await args.command;
+        if (cmd && cmd.name !== 'help') return this.helpCommand(msg, cmd)
+        else this.helpAll(msg)
+    }
+
+    helpAll(msg) {
         const commands = Array.from(this.client.registry.commands.collection.values())
-        // console.log(commands.map(cmd => `\`${cmd.name}\` ${cmd.description ? `: ${cmd.description}` : ''}`).join("\n"))
         const embed = new MessageEmbed()
-            .setColor('#EA2027')
+            .setColor('RANDOM')
             .setTitle(`Message d'aide`)
             .setFooter(this.client.user.username)
             .setTimestamp(new Date())
             .addField('Commandes', commands.map(cmd => `\`${this.client.prefix}${cmd.name}\` ${cmd.description ? `: ${cmd.description}` : ''}`).join("\n"))
-        msg.reply(embed)
+        msg.reply(embed);
+    }
+
+    helpCommand(msg, command) {
+        const args = command.argsCollector ? command.argsCollector.args : [];
+        const formattedArgs = (args.length > 0) ? args.map(arg => arg.default ? `[${arg.label}=${arg.default}]` : `<${arg.label}>`).join(' ') : '';
+        const embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle(`Message d'aide - Commande \`${this.client.prefix}${command.name}\``)
+            .setFooter(this.client.user.username)
+            .setTimestamp(new Date())
+            .addField('Commande', `\`${this.client.prefix}${command.name}${args.length > 1 ? ` ${formattedArgs}` : ''}\` ${command.description ? `: ${command.description}` : ''}`)
+        if (command.alias.length > 0) {
+            embed.addField('Alias', command.alias.map(alias => `\`${this.client.prefix}${alias}\``).join(', '))
+        }
+        msg.reply(embed);
     }
 }
 
