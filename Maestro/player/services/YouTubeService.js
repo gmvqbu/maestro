bb8c79f5c40bd98069d0ff1d14d9a100d758dc46
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-27 20:32:30 +0100

    maj : YouTubeService
    Cablage depuis l'API vers le player

--- Maestro/player/services/YouTubeService.js
+++ Maestro/player/services/YouTubeService.js
@@ -1,6 +1,14 @@
 'use strict';
 
+/**
+ * NOTE
+ * Each day you have a quota of 10 000 units to interact with the Google API
+ * Fetch a video, channel or playlist from an id cost 1 point
+ * Search a video, channel or playlist cost 100 points
+ */
+
 const YouTubeDataAPI = require("../apis/YouTubeDataAPI");
+const BaseTrack = require("../tracks/BaseTrack");
 const YouTubeTrack = require("../tracks/YouTubeTrack");
 
 class YouTubeService {
@@ -13,13 +21,8 @@ class YouTubeService {
         this.api = new YouTubeDataAPI()
     }
 
-    static async video(msg, query) {
-        // console.log(msg.embeds)
-        // Récupérer les infos des vidéos depuis l'embed du message
-        // Sinon, effectuer une recherche avec l'API
-        // const data = await this.api.fetchVideoFromString(query)[0]
-        // en attendant, je charge mon fichier json pour pas manger tout mon quota
-        const data = require('../../../config/tmp.yt.search.json').items[0];    // temp
+    static async video(query) {
+        const data = await this.api.fetchVideoFromSearch(query);
         return new YouTubeTrack(data);
     }
 }
