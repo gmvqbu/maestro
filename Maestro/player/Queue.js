b025519c83d5c7d5bd3666063669b18e633f70ca
gmvqbu <gmvqbu@users.noreply.github.com>
2021-02-27 14:34:25 +0100

    add : Queue.js
    Ajout de la classe Queue (liste de lecture)

---
+++ Maestro/player/Queue.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** Represent the music queue */
+class Queue extends Array {
+    loop = 'off';
+
+    /** Empty the queue */
+    empty() {
+        return this.splice(0, this.length);
+    }
+
+    /** Return next track */
+    next() {
+        const track = this.shift();
+        return track;
+    }
+
+    /** Add a track to the queue */
+    add(track) {
+        return this.push(track);
+    }
+
+    /** Shuffle this queue */
+    shuffle() {
+        for (let i = this.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this[i], this[j]] = [this[j], this[i]];
+        }
+        return this;
+    }
+}
+
+module.exports = Queue;
